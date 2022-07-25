b82f883c4391ee4e4d8ca24a60e90c7dd810c072
BurritoCarlito <carlotanjuaquio@gmail.com>
2022-07-24 18:54:50 -0700

    implemented DayListItem

--- stories/index.js
+++ stories/index.js
@@ -2,10 +2,26 @@ import React from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
+import DayListItem from "components/DayListItem";
 
 import "index.scss";
 
 import Button from "components/Button";
+
+storiesOf("DayListItem", module) 
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => <DayListItem name="Monday" spots={5} />) // To 
+  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
+  .add("Full", () => <DayListItem name="Monday" spots={0} />)
+  .add("Clickable", () => (
+    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> 
+  ));
+
+
+
+
 
 storiesOf("Button", module)
   .addParameters({
