f64e690cdb011be97cd9ea28668b9ea3bf56ef45
BurritoCarlito <carlotanjuaquio@gmail.com>
2022-08-05 23:33:42 -0700

    added edit appointment functionality

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -15,6 +15,7 @@ const CREATE = "CREATE";
 const SAVING = "SAVING";
 const DELETING = "DELETING";
 const CONFIRM = "CONFIRM";
+const EDIT = "EDIT";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
@@ -42,7 +43,11 @@ export default function Appointment(props) {
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
-        <Show student={props.interview.student} interviewer={props.interview.interviewer} onDelete={() => transition(CONFIRM)} />
+        <Show 
+          student={props.interview.student} 
+          interviewer={props.interview.interviewer} onDelete={() => transition(CONFIRM)}
+          onEdit={() => transition(EDIT)}
+           />
       )}
       {mode === CREATE && (
         <Form interviewers={props.interviewers} onCancel={() => back(EMPTY)} onSave={save} />
@@ -55,6 +60,15 @@ export default function Appointment(props) {
       )}
       {mode === DELETING && (
         <Status message={"Deleting"} />
+      )}
+      {mode === EDIT && (
+        <Form 
+        student={props.interview.student} 
+        interviewer={props.interview.interviewer}
+        interviewers={props.interviewers}
+        onSave={save} 
+        onCancel={() => back(SHOW)}
+         />
       )}
     </article>
   );
