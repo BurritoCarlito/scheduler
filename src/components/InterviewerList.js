2432e9571a16e63bc402c4804c5a7943acf7bf95
BurritoCarlito <carlotanjuaquio@gmail.com>
2022-08-12 14:17:18 -0700

    implemented prop-types, passing tests

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss"
+import PropTypes from "prop-types";
 
 export default function InterviewerList(props) { 
 
@@ -22,5 +23,9 @@ export default function InterviewerList(props) {
   <ul className="interviewers__list">{interviewers}</ul>
 </section>
   );
+
+  InterviewerList.PropTypes = {
+    interviewers: PropTypes.array.isRequired
+  };
 
 }
